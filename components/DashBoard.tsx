94b3ddc2256ccfaff7cf8b0fa598b57ba11e6988
daichi-sato-design <daichisato.design@gmail.com>
2022-10-09 21:39:54 +0900

    ダッシュボードにメニューを作成

--- components/DashBoard.tsx
+++ components/DashBoard.tsx
@@ -1,7 +1,9 @@
 import { FC } from 'react'
 import { ShieldCheckIcon } from '@heroicons/react/solid'
-import { LoginIcon, LogoutIcon } from '@heroicons/react/outline'
+import { LogoutIcon } from '@heroicons/react/outline'
 import { ActionIcon, Center, Menu } from '@mantine/core'
+import { NextLink } from '@mantine/next'
+import { Settings } from 'tabler-icons-react'
 import { supabase } from '../utils/supabase'
 import { Layout } from './Layout'
 
@@ -14,6 +16,39 @@ export const DashBoard: FC = () => {
     <Layout title="DashBoard">
       <Center>
         <ShieldCheckIcon className="mb-4 h-14 w-14 text-teal-500" />
+      </Center>
+      <Center>
+        <Menu trigger="hover" size="xl">
+          <Menu.Label>UI Components</Menu.Label>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/button"
+          >
+            Button
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/grid"
+          >
+            Grid
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/group"
+          >
+            Group
+          </Menu.Item>
+          <Menu.Item
+            icon={<Settings size={16} />}
+            component={NextLink}
+            href="/multi-select"
+          >
+            Multi Select
+          </Menu.Item>
+        </Menu>
       </Center>
       <Center>
         <ActionIcon my="md" size="lg" onClick={signOut}>
