882f730c012ccecb0dfac8cda4b0b2853c24a1c4
daichi-sato-design <daichisato.design@gmail.com>
2022-10-10 16:53:38 +0900

    DataLoaderページの作成

---
+++ pages/fetch-api.tsx
@@ -0,0 +1,28 @@
+import { NextPage } from 'next'
+import Link from 'next/link'
+import { Center, Grid } from '@mantine/core'
+import { ReplyIcon } from '@heroicons/react/solid'
+import { Layout } from '../components/Layout'
+import { FetchTodos } from '../components/FetchTodos'
+
+const FetchData: NextPage = () => {
+  return (
+    <Layout title="Fetch Data Demo">
+      <Grid>
+        <Grid.Col span={6}>
+          <FetchTodos />
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <FetchTodos />
+        </Grid.Col>
+      </Grid>
+      <Center>
+        <Link href="/">
+          <ReplyIcon className="mt-4 h-6 w-6 cursor-pointer text-gray-300" />
+        </Link>
+      </Center>
+    </Layout>
+  )
+}
+
+export default FetchData
